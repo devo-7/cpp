671f177e31536cd6637c132671bc31bd54187abe
dovlet_sydv <vchomsilabrat1@gmail.com>
2026-04-13 13:39:26 +0300

    Refactor division logic with input validation

--- 6.cpp
+++ 6.cpp
@@ -1,11 +1,16 @@
 #include<iostream>
 main(){
 	int s1,s2,s=0;
+	a1:
 	printf("1 sayiyi gir:");
 	scanf("%d",&s1);
 	printf("2 sayiyi gir:");
 	scanf("%d",&s2);
-	for(int i=0;i<s2;i++){
+	if(s1<s2){
+		printf("1 sayi 2 sayidan kucuk\n");
+		goto a1;
+	}
+	while(s1>=s2){
 		s1=s1-s2;
 		s++;
 		if(s1<s2){
